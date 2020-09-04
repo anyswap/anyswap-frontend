f5263c1879295bbbf44a5b6f5e4a171ba4b82216
winter520 <WDwq681227>
2020-09-04 15:04:26 +0800

    open config

---
+++ src/config/index.js
@@ -0,0 +1,44 @@
+
+import coin from './coinbase/coin'
+import getFSNConfig from './coinbase/fusion'
+import getBNBConfig from './coinbase/binance'
+// console.log(coin)
+// const ENV_CONFIG = 'BNB_MAIN'
+const ENV_CONFIG = 'BNB_TEST'
+// const ENV_CONFIG = 'FSN_TEST'
+// const ENV_CONFIG = 'FSN_MAIN'
+let netArr = ENV_CONFIG.split('_')
+let netConfig = {}
+if (netArr[0] === 'FSN') {
+  netConfig = getFSNConfig(netArr[1])
+} else if (netArr[0] === 'BNB') {
+  netConfig = getBNBConfig(netArr[1])
+}
+
+// /**
+//  * @description 配置以太坊网络节点
+//  */
+let ercConfig = {
+  chainID: 4,
+  nodeRpc: 'https://rinkeby.infura.io/v3/0e40cfd5e7a64b2d9aea8427e4bd52a0',
+  lookHash: 'https://rinkeby.etherscan.io/tx/'
+}
+if (netArr[1].toLowerCase() === 'test') {
+  ercConfig = {
+    chainID: 1,
+    // nodeRpc: 'https://mainnet.infura.io/v3/0e40cfd5e7a64b2d9aea8427e4bd52a0',
+    nodeRpc: 'https://ethmainnet.anyswap.exchange',
+    lookHash: 'https://etherscan.io/tx/'
+  }
+}
+
+export default {
+  ...netConfig,
+  ...coin,
+  env: netArr[1].toLowerCase(),
+  supportWallet: ['Ledger'],
+  reg: {
+    [coin.BTC]: /^[13][0-9a-zA-Z]{25,33}$|^[2mn][0-9a-zA-Z]{25,34}$/
+  },
+  ercConfig,
+}
