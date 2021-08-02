3bc133bb71fdd74b1dfc213746614a6d929e4151
d_winter <2624376436@qq.com>
2021-08-02 17:23:47 +0800

    Update FarmsList.js

--- src/pages/Farms/FarmsList.js
+++ src/pages/Farms/FarmsList.js
@@ -559,6 +559,7 @@ export default function FarmsList () {
         <img src={require('../../assets/images/banner/farm.png')} />
       </BannerBox> */}
       <FarmListBox>
+        {farmItem(1, 1, 'https://stable.anyswap.exchange/#/farm/bsc', 'ANY Farming', 'ANY Farming', 'ANY', 'BNB')}
         {farmItem(1, 1, 'https://app.wault.finance/bsc/#farm', 'WAULTx Farming', 'WAULTx Farming', 'DEP1', 'BNB')}
         {farmItem(1, 1, 'https://kuswap.io/#/farms', 'KUS Farming', 'KUS Farming', 'KUS', 'KCC')}
         {farmItem(1, 1, 'https://pancakeswap.info/pool/0xB87b857670A44356f2b70337E0F218713D2378e8', 'ORBS Farming', 'ORBS Farming', 'ORBS', 'BNB')}
